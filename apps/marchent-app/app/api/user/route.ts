479db6de1bfdb556a59168721c4ba4b0a67ea738
manishnangliya <manunangliya@gmail.com>
2024-06-13 00:06:20 +0530

    added workflow and fixed build issues

--- apps/marchent-app/app/api/user/route.ts
+++ apps/marchent-app/app/api/user/route.ts
@@ -1,16 +1,7 @@
-import {PrismaClient} from '@repo/db/client'
-import { NextResponse } from 'next/server';
 
-const client = new PrismaClient();
+import { NextResponse } from "next/server";
 
 export const GET = async () => {
-    const user = await client.user.create({
-        data: {
-            email: "newemail@gmail.com",
-            password: "newpass"
-        }
-    })
-    // console.log(user);
     return NextResponse.json({
         message: "hi there"
     })
--- apps/marchent-app/app/page.tsx
+++ apps/marchent-app/app/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@repo/ui/button";
+
 
 
 export default function Page(): JSX.Element {
   return (
-    <div className="text-5xl">hello
-    <Button className="facd" appName="button">Button</Button></div>
+    <div>Marchent App</div>
   );
 }
